b9cdd8a537f886622fdc43f0de64579cb7f5a595
Thiago Sousa <155773063+tpsousa@users.noreply.github.com>
2025-01-22 21:03:23 -0300

    a first commit of the project

---
+++ model/model.ts
@@ -0,0 +1,28 @@
+//modelo do banco de dados para receber o arquivo -->nao sei se e o mesmo de transformar o arquivo em json
+
+const createDatabase = {
+      
+}
+
+const arquivo  = { 
+    user: 'thiago',
+    password : 'key',
+    localhost: 'https://localhost:2010',
+}
+
+const user = {
+    index:(req,res) =>{
+        const server = async(await)
+    },
+
+    createUser (name,password,email) {
+       this.password = password;
+       this.email = email;
+       this.name = name;
+    
+       if(password != password ){
+
+       }
+
+    }
+}
---
+++ process.c
@@ -0,0 +1,51 @@
+//ler o arquivo e implementar o codigo para encontrar o clique maximo....
+
+int arquivo json = '';
+
+int verificar_arquivo( *arquivo_json){
+ //verificar a existencia do arquivo
+}
+
+
+struct dfs {
+
+  int adj[10][10];
+x
+};
+
+typedef *linked_list {
+
+}
+
+//funcao para criar aresta
+//funcao para remover arestar
+//funcao para mostrar uma aresta
+
+int matrix[4][10];
+
+matrix = { 
+    {0,2,3,2,1,2,3,3,4,2},
+    {1,2,3,3,4,4,5,6,6,5},
+    {2,1,2,1,2,3,1,2,3,1},
+    {2,3,4,2,1,3,4,5,3,1},
+};
+//criar uma matriz de adjacencias;
+//search the maximal connection of vertices -----> 
+
+int maximal(int *arr,int size) {
+    for(int i = 0 ; i < size ; i++) {
+    for(int j=i+1; j < size; j++){
+
+    }
+}
+}
+
+
+free(dfs);
+
+return 0; 
+
+}
+
+//implementacao basica de uma dfs
+.......
