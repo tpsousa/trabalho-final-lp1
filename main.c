ff2852fdcc79f97251f304710f7d159bc5306905
Thiago Sousa <155773063+tpsousa@users.noreply.github.com>
2025-02-04 10:11:09 -0300

    wheres the code

---
+++ main.c
@@ -0,0 +1,69 @@
+
+#include <stdio.h>;
+#include <stdio.h>;
+
+#define MAX_VERTICES 1000
+
+struct NODE{
+    int node;
+    node * next;
+}
+
+typedef struct {
+    int adjacency_list[MAX_VERTICES];
+    int node[MAX_VERTICES][500]; 
+    int node_count[MAX_VERTICES]; //um vetor responsavel por guardar o grau de cada vertice
+}ComunityGraph;
+
+//inicializar um grafo vazio
+void initialize_graph(ComunityGraph * node){
+  g-> inicio =0;
+  g->fim = null;
+
+  for(int i = 0 ; i < node_count; i++){
+
+  }
+}
+
+//adiciona uma aresta entre dois vertices
+void add_egde(ComunityGraph * node,count_node) {
+ 
+}
+//imprime o grafo na tela
+void mostrarGrafo(ComunityGraph * node) {
+  for(int i = 0 ; i <  node_count; i ++){
+
+  }
+}
+
+//encontrar o vertice inicial que possue o maior numero de conexoes
+//e a partir dele encontrar os outros vertices de maior grau
+//e verificar se esses vertices estao conectados com o usuario inicial
+void user(ComunityGraph * node){
+  
+}
+//contar o grau de cada no e botar em uma classe de armazenamento
+void count_node(ComunityGraph * node,count_node) {
+ for(int i=0 ; i < node_count; i++){
+   if(i != [temp->node]-next){
+    count_node++;
+   }    
+ }
+}
+
+int main () {
+ initialize_graph();
+
+
+ //chamar uma funcao que le e executa o arquivo
+  fs....
+
+  //liberar o espaco de memoria do grafo
+  free(graph);
+
+  return  0;
+}
+
+
+
+
